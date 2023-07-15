27c796b6f6a8789c6f48ed83f0232c2cadcf1a70
neverSettles <christopherRSettles@gmail.com>
2023-07-15 13:19:18 -0700

    Add server

---
+++ src/server.py
@@ -0,0 +1,19 @@
+import openai
+import os
+
+from flask import Flask, jsonify, request
+from dotenv import load_dotenv
+
+
+# Set up OpenAI API credentials
+load_dotenv()
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return jsonify({"Choo Choo": "Make a post request to /suggest to get output 🚅"})
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=os.getenv("PORT", default=5001))
