4e8b6b0eb6f6e1184bb2cbe0207ddbdbe09d5ab0
neverSettles <christopherRSettles@gmail.com>
2023-07-15 17:22:19 -0700

    Add eleven labs code

--- src/generate/gen_podcast.py
+++ src/generate/gen_podcast.py
@@ -5,20 +5,10 @@ import io
 import time
 import argparse
 from dotenv import load_dotenv
+
+from eleven import elevenlabs
 load_dotenv()
 openai.api_key = os.getenv("OPENAI_API_KEY")
-
-# def call_openai_api(prompt):
-#     message=[{"role": "user", "content": prompt}]
-#     response = openai.ChatCompletion.create(
-#         model="gpt-4",
-#         messages = message,
-#         temperature=0.2,
-#         max_tokens=1000,
-#         frequency_penalty=0.0
-#     )
-
-#     return response.choices[0].text.strip()
 
 
 def call_openai_api(prompt):
@@ -41,9 +31,8 @@ def call_openai_api(prompt):
 
     return generated_text
 
-
-def synthesize_speech(text):
-    # Create a client using your AWS access keys stored as environment variables
+def sythesize_speech_aws(text):
+        # Create a client using your AWS access keys stored as environment variables
     polly_client = boto3.Session(
                     aws_access_key_id=os.getenv('AWS_ACCESS_KEY'),
                     aws_secret_access_key=os.getenv('AWS_SECRET_KEY'),
@@ -59,24 +48,38 @@ def synthesize_speech(text):
     with open(filename, 'wb') as file:
         file.write(response['AudioStream'].read())
 
+def synthesize_speech(text):
+    # Create a client using your AWS access keys stored as environment variables
+
+    # elevenlabs.convert_to_speech(text, 'output/speech')
+    sythesize_speech_aws(text)
+    print('output saved in output/speech.mp3')
+
 
 def create_podcast_prompt(topic, duration):
     # Create the podcast prompt
-    meta_prompt = f"""
-Please help me to make a prompt to GPT-3 to generate a podcast about {topic}.
-The prompt should instruct GPT that the podcast should be {duration} minutes long.
-The prompt should instruct GPT to make the podcast seem like a fluid conversation, without breaks in the conversation.
-The prompt should instruct GPT that the text of the response should be the transcript of the podcast.
-There should be no seperator between the segments, so that the podcast is one continuous audio file.
-Please only output a prompt that I can use to send to GPT.
-"""
-    return meta_prompt
+#     meta_prompt = f"""
+# Please help me to make a prompt to GPT-3 to generate a podcast about {topic}.
+# The prompt should instruct GPT that the podcast should be {duration} minutes long.
+# The prompt should instruct GPT to only reply with the text of the podcast it generates.
+# The prompt should instruct GPT that the podcast would be with 1 person only, and not try to switch between multiple people.
+# The prompt should instruct GPT to make the podcast seem like a fluid conversation, without breaks in the conversation.
+# The prompt should instruct GPT that the text of the response should be the transcript of the podcast.
+# There should be no seperator between the segments, so that the podcast is one continuous audio file.
+# Please only output a prompt that I can use to send to GPT.
+# """
+    prompt = f"""
+Create the audio transcript of a podcast about {topic}.
+The podcast should be {duration} minutes long.
+The podcast should be with 1 person only, and not try to switch between multiple people.
+The podcast should seem like a fluid conversation, without breaks in the conversation.
+The text of the response should be the transcript of the podcast.
+There should be no seperator between the segments, so that the podcast is one continuous audio file."""
+    return prompt
 
 def create_podcast(topic, duration):
-    meta_prompt = create_podcast_prompt(topic, duration)
-    print(meta_prompt)
+    prompt = create_podcast_prompt(topic, duration)
 
-    prompt = call_openai_api(meta_prompt)
     print(prompt)
 
     story = call_openai_api(prompt)
@@ -90,8 +93,8 @@ def create_podcast(topic, duration):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Podcast Generator')
-    parser.add_argument('-t', '--topic', help='Topic of the podcast')
-    parser.add_argument('-d', '--duration', help='Duration of the podcast in minutes')
+    parser.add_argument('-t', '--topic', required=True,  help='Topic of the podcast')
+    parser.add_argument('-d', '--duration', required=True, help='Duration of the podcast in minutes')
     args = parser.parse_args()
     topic = args.topic
     duration = args.duration
