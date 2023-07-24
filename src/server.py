d4ecbfbe19669cbed32e8a5b4074d485bc168bc9
neverSettles <christopherRSettles@gmail.com>
2023-07-23 20:23:16 -0700

    Change server to run on port 5000

--- src/server.py
+++ src/server.py
@@ -89,4 +89,4 @@ def create_post_demo():
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=os.getenv("PORT", default=5001))
+    app.run(debug=True, host='0.0.0.0', port=os.getenv("PORT", default=5000))
